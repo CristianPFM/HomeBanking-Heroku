c89e43cd0f294ca68dd4650049953dd19d37706c
CristianPFM <83243697+CristianPFM@users.noreply.github.com>
2022-04-04 10:36:03 -0400

    agregado homecontroller

---
+++ src/main/java/com/mindhub/homebanking/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.mindhub.homebanking.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView mostrarHome(){
+        ModelAndView model = new ModelAndView("redirect:web/ecobank/index.html");
+        return model;
+    }
+}
